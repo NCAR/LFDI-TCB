c175fae16654aca4bf5a7951faf47ef845be3fd7
Mitch Jeffers <mitchem890@gmail.com>
2023-01-22 21:34:58 -0700

    Updated Functions with actual data

--- Core/Inc/TemperatureCorrectionTable.h
+++ Core/Inc/TemperatureCorrectionTable.h
@@ -7,31 +7,7 @@
  */
 
 
-//This Will translate a nm Tuning request to a position at 25C
-//The Range Should Cover 600nm to 700nm in 0.01 nm intervals or 10000 entries
-uint16_t NM_to_Position(float nm)
-{
-//Index starts at 600 and moves to 700 in .01 intervals
-//Convert the User input by subtracting 600 and muliplying by 10000
-uint16 adjusted_input = (nm - 600)*10000;
-static const __flash uint16_t lookup[10000] = {0};
-pos = lookup[nm];
-return ps;
-}
-
-
-//Lookup Table to convert a pixel position to a voltage at the base Temperature. 25C
-uint16 BaseT_Position_to_BaseT_Voltage(uint16 position){
-	static const __flash uint16_t lookup[15000] =
-	{0
-	};
-	volts = lookup[volts];
-	return volts;
-}
-
-//This Function Will Convert the temperature to a positional offset
-//This is based on the Found relation ship between Position and Temperature
-uint16_t temperature_position_offset(float* temp){
-	offset = (uint16_t)(43.06*(&temp)-985.5);
-	return offset;
-}
+uint16_t NM_to_Position(float nm);
+float BaseT_Position_to_BaseT_Voltage(uint16_t position);
+uint16_t temperature_position_offset(float* temp);
+float Wavelength_to_Voltage(float* wavelength, float* temp);
