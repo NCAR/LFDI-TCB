355933d03c6c32119a186c5ecc9195614f18ca20
mitchem890 <mitchem890@gmail.com>
2024-11-01 14:51:01 -0600

    Updated Sensors

--- Core/Src/TuningControlBoard.c
+++ Core/Src/TuningControlBoard.c
@@ -14,11 +14,11 @@ void TCB_InitStruct(struct sTuningControlBoard* s, I2C_HandleTypeDef* hi2c1,I2C_
     //For each of the Seven Temperature sensors initialize the struct
     for(int i = 0; i < NUMOFSENSORSBUS1; i++){
         //The Last parameter is the address of the sensor this will need to be changed as we get more sensors
-        TMP117_InitStruct(&s->Sensor[i], hi2c1, 0x00);
+        TMP117_InitStruct(&s->Sensor[i], hi2c1, 0x00+i);
     }
     for(int i = 0; i < NUMOFSENSORSBUS2; i++){
         //The Last parameter is the address of the sensor this will need to be changed as we get more sensors
-        TMP117_InitStruct(&s->Sensor[i+NUMOFSENSORSBUS1], hi2c2, 0x00);
+        TMP117_InitStruct(&s->Sensor[i+NUMOFSENSORSBUS1], hi2c2, 0x00+i);
     }
     //Initialize the DAC
     DAC_InitStruct(&s->DAC8718, hspi);
@@ -26,14 +26,14 @@ void TCB_InitStruct(struct sTuningControlBoard* s, I2C_HandleTypeDef* hi2c1,I2C_
     //Initialize the Controller
     //All of these are set up to use the first sensor this will need to be changed as we get more sensors
     for (int i = 0; i < NUMOFHEATERCONTROLLERS; i++){
-        HeaterController_InitStruct(&s->HeaterControllers[i], &s->Sensor[4], i+1);
+        HeaterController_InitStruct(&s->HeaterControllers[i], &s->Sensor[i], i+1);
     }
     
     //Initialize the Compensator
     //Todo this will need to have different sensors
     float StageSizes[NUMOFCOMPENSATORS] = {STAGE1, STAGE2, STAGE3, STAGE4, STAGE5, STAGE6};
     for(int i = 0; i < NUMOFCOMPENSATORS; i++){
-        Compensator_InitStruct(&s->Compensator[i], &s->Sensor[4], &s->DAC8718.DAC_Channels[i], StageSizes[i]);
+        Compensator_InitStruct(&s->Compensator[i], &s->Sensor[i], &s->DAC8718.DAC_Channels[i], StageSizes[i]);
     }
 
     //Initialize the GPIO
