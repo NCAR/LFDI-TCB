1bde6ce0e9ba090d9edea8761ddcebe255304df7
Mitch Jeffers <mitchem890@gmail.com>
2026-02-02 20:47:37 -0500

    Added Comment

--- Core/Src/CurrentSensor.c
+++ Core/Src/CurrentSensor.c
@@ -19,6 +19,7 @@ void CurrentSensor_InitStruct(struct sCurrentSensor* s, ADC_HandleTypeDef* inter
     s->Current = 0.0f;
     s->SamplesInAverage = 77;  // Average over 10 seconds (77 samples × 130ms = 10.01 seconds)
     s->State = CURRENTSENSOR_STATE_UNKNOWN;
+    // Initialize the array with 80 samples
     for (i = 0; i < 80; i++)
         s->Currents[i] = 0.0f;
     s->Errors = 0;
